b48ea7f5131b455e13d19ce257d99bb36a358b3c
iburadempa <ibu@radempa.de>
2015-11-16 19:34:35 +0100

    add setup.py to allow installing with pip3 install -e

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pg_jts',
+      version='0.0.1',
+      description='Create JSON-table-schema from a live PostgreSQL database',
+      url='http://github.com/iburadempa/pg_jts',
+      author='ibu radempa',
+      author_email='ibu@radempa.de',
+      license='MIT',
+      packages=['pg_jts'],
+      zip_safe=False)
+
